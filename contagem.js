957f3ebb1a1d6dd8ba14dc0656ca2c64ac064e1e
Luciano-do-Carmo <lucianodocarmo111@gmail.com>
2022-01-30 11:37:51 -0300

    adicionando arquivos percorrer, contagem e exercicio

---
+++ contagem.js
@@ -0,0 +1,14 @@
+//contagem do zero ate o 9
+let numeroalunos = 10;
+for(let contador = 0; contador < numeroalunos; contador ++)
+
+//numeros pares e impares e verificação do ZERO
+if(contador==0){
+  console.log (" O número é ZERO")
+}
+else if(contador % 2 == 0) {
+  console.log(` O numero ${contador} é PAR`);
+}
+else if(contador %2 == 1) {
+  console.log(` O numero ${contador} é IMPAR`);
+}
---
+++ percorrer.js
@@ -0,0 +1,6 @@
+//contagem do zero ate o 9
+let numeroalunos = 10;
+
+for(let contador = 0; contador < numeroalunos; contador ++) {
+    console.log(contador)
+}
